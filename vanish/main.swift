cda847668136ef9b9a39dd268d0cd48076e9ddba
toastrr <brendanballon@gmail.com>
2020-06-27 15:30:43 -0400

    Initial Commit

---
+++ vanish/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  vanish
+//
+//  Created by Brendan Ballon on 6/27/20.
+//  Copyright © 2020 Brendan Ballon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
